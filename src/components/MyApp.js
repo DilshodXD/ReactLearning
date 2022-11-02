1aaf5f2f14b705990e8eb3dca38832d92ac4b8b4
Dilshod <dilshodxdxd@gmail.com>
2022-11-02 15:55:35 +0500

    learn react componets

---
+++ src/components/MyApp.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './styles.css'
+
+export default class MyApp extends React.Component {
+    render() {
+        return(
+            <>
+                <h1 className="h1">Hello React</h1>
+                <p>lorem ipsum</p>
+            </>
+        )
+    }
+}
---
+++ src/components/MyApp2.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function MyApp2() {
+    return (
+        <>
+            <div>
+                <h2>Hello react 2</h2>
+                <p>lorem ipsum 2 </p>
+            </div>
+        </>
+    )
+}
