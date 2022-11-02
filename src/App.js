5a1f953a248820e32e9eb461d35e3ca670de4c47
Dilshod <dilshodxdxd@gmail.com>
2022-11-02 17:10:53 +0500

    commited props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import MyApp from './components/MyApp'
-import MyApp2 from './components/MyApp2';
+import MyApp2 from './components/MyApp2'
+import Component1 from './propsTutorial/component1'
+import Component2 from './propsTutorial/component2'
 
 function App() {
   return (
     <div>
+      <Component1 age="17" name="Dilshod"/>
+      <Component2  text="React Tutorial"/>
+
       {/* <MyApp/>  
       <MyApp2/>   */}
     </div>
---
+++ src/propsTutorial/component2.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function component2(props) {
+    return (
+        <div>
+        <p>This is {props.text}</p>
+        </div>
+    );
+}
+
+export default component2;
